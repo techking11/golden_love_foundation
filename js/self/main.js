c5a874e4ed982133f9d50edc5d9081962b42be40
techking11 <techking1132000@gmail.com>
2023-05-06 14:24:12 +0630

    fist-commit

---
+++ js/self/main.js
@@ -0,0 +1,9 @@
+// --------------- Back to top button -------------->
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
